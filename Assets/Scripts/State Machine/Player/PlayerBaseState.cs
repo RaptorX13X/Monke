189c5a52155822eaa0051cd32dca1e150e9856b0
Raptor <panraptor13@gmail.com>
2024-12-12 18:38:58 +0100

    slopehop fix

--- Assets/Scripts/State Machine/Player/PlayerBaseState.cs
+++ Assets/Scripts/State Machine/Player/PlayerBaseState.cs
@@ -11,13 +11,31 @@ public abstract class PlayerBaseState : State
     
     protected void Move(Vector3 motion, float deltaTime)
     {
+        motion = AdjustVelocityToSlope(motion);
         stateMachine.CharacterController.Move((motion + stateMachine.ForceReceiver.Movement) * deltaTime);
         
         stateMachine.BaseTransform.rotation = Quaternion.Euler(stateMachine.BaseTransform.rotation.eulerAngles.x, stateMachine.MainCameraTransform.rotation.eulerAngles.y, stateMachine.BaseTransform.rotation.eulerAngles.z);
     }
 
-    protected void ReturnToLocomotion()
+    protected void ReturnToLocomotion() 
     {
         stateMachine.SwitchState(new PlayerFreeLookState(stateMachine));
+    }
+    
+    private Vector3 AdjustVelocityToSlope(Vector3 velocity)
+    {
+        var ray = new Ray(stateMachine.BaseTransform.position, Vector3.down);
+
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, 5f))
+        {
+            var slopeRotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+            var adjustedVelocity = slopeRotation * velocity;
+
+            if (adjustedVelocity.y < 0)
+            {
+                return adjustedVelocity;
+            }
+        }
+        return velocity;
     }
 }
