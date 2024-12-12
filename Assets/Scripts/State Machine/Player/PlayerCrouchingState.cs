0fd8e734d73281d275a6b89cf2772df72c952ce9
Raptor <panraptor13@gmail.com>
2024-12-12 17:44:29 +0100

    strafing

--- Assets/Scripts/State Machine/Player/PlayerCrouchingState.cs
+++ Assets/Scripts/State Machine/Player/PlayerCrouchingState.cs
@@ -34,14 +34,14 @@ public class PlayerCrouchingState : PlayerBaseState
     private Vector3 CalculateMovement()
     {
         Vector3 forward = stateMachine.MainCameraTransform.forward;
-        //Vector3 right = stateMachine.MainCameraTransform.right;
+        Vector3 right = stateMachine.MainCameraTransform.right;
 
         forward.y = 0f;
-        //right.y = 0f;
+        right.y = 0f;
 
         forward.Normalize();
-        //right.Normalize();
-        return forward * stateMachine.InputReader.MovementValue; // + right * stateMachine.InputReader.MovementValue.x;
+        right.Normalize();
+        return forward * stateMachine.InputReader.MovementValue.y  + right * stateMachine.InputReader.MovementValue.x;
     }
 
     private void OnJump()
