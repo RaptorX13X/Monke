c522308757c16c2757ab2dae8862b7b9ff7474e3
Raptor <panraptor13@gmail.com>
2024-12-22 22:21:11 +0100

    .

--- Assets/Scripts/State Machine/Player/PlayerBaseState.cs
+++ Assets/Scripts/State Machine/Player/PlayerBaseState.cs
@@ -16,9 +16,9 @@ public abstract class PlayerBaseState : State
         
         stateMachine.BaseTransform.rotation = Quaternion.Euler(stateMachine.BaseTransform.rotation.eulerAngles.x, stateMachine.MainCameraTransform.rotation.eulerAngles.y, stateMachine.BaseTransform.rotation.eulerAngles.z);
 
-        if (stateMachine.lastFootstepTime <= 0f)
+        if (Time.time - stateMachine.lastFootstepTime > 0.5f)
         {
-            stateMachine.lastFootstepTime = 0.5f;
+            stateMachine.lastFootstepTime = Time.time; // check if its not too messy
             stateMachine.PlayerAudio.PlayFootsteps();
         }
     }
