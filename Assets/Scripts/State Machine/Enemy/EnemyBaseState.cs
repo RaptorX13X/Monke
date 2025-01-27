6d54cdca79a5e66116239e1021b149b21c49b1eb
Raptor <panraptor13@gmail.com>
2025-01-27 12:02:40 +0100

    .

--- Assets/Scripts/State Machine/Enemy/EnemyBaseState.cs
+++ Assets/Scripts/State Machine/Enemy/EnemyBaseState.cs
@@ -27,11 +27,29 @@ public abstract class EnemyBaseState : State
     }
     protected void Move(Vector3 motion, float deltaTime)
     {
+        motion = AdjustVelocityToSlope(motion);
         stateMachine.CharacterController.Move((motion + stateMachine.ForceReceiver.Movement) * deltaTime);
     }
 
     protected void Move(float deltaTime)
     {
         Move(Vector3.zero, deltaTime);
+    }
+    
+    private Vector3 AdjustVelocityToSlope(Vector3 velocity)
+    {
+        var ray = new Ray(stateMachine.transform.position, Vector3.down);
+
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, 5f))
+        {
+            var slopeRotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+            var adjustedVelocity = slopeRotation * velocity;
+            
+            if (adjustedVelocity.y < 0)
+            {
+                return adjustedVelocity;
+            }
+        }
+        return velocity;
     }
 }
