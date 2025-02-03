26d94b8d2361561a81146a62775e4ac18623b87d
Raptor <panraptor13@gmail.com>
2025-02-03 23:41:01 +0100

    puzzles

--- Assets/Scripts/PressurePlateCheat.cs
+++ Assets/Scripts/PressurePlateCheat.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 public class PressurePlateCheat : MonoBehaviour
 {
     [SerializeField] private GameObject objectToDisable;
+    [SerializeField] private Transform center;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("PuzzleBlock"))
         {
+            other.transform.position = center.position;
             objectToDisable.SetActive(false);
         }
     }
