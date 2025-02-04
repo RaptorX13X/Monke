a65cfa57cc150b61b3c50b5579ca0041410f9f41
Raptor <panraptor13@gmail.com>
2025-02-04 23:20:46 +0100

    menu script

---
+++ Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] private int gameSceneNumber;
+    [SerializeField] private GameObject menuObject;
+    [SerializeField] private GameObject controlsObject;
+    [SerializeField] private int screenWidth = 640;
+    [SerializeField] private int screenHeight = 480;
+
+    private void Awake()
+    {
+        menuObject.SetActive(true);
+        controlsObject.SetActive(false);
+        Screen.SetResolution(screenWidth, screenHeight, FullScreenMode.Windowed);
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadSceneAsync(gameSceneNumber);
+    }
+
+    public void ControlsButton()
+    {
+        menuObject.SetActive(false);
+        controlsObject.SetActive(true);
+    }
+
+    public void BackToMenuButton()
+    {
+        menuObject.SetActive(true);
+        controlsObject.SetActive(false);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+    
+    
+}
