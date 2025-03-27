638c0782109c419ff0d5a30873c59dbbd4e31955
Raptor <panraptor13@gmail.com>
2025-03-27 15:39:26 +0100

    .

---
+++ Assets/Scripts/State Machine/Player/PlayerTransformationState.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerTransformationState : PlayerBaseState
+{
+    public PlayerTransformationState(PlayerStateMachine stateMachine) : base(stateMachine)
+    {
+    }
+
+    public override void Enter()
+    {
+        
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        
+    }
+
+    public override void Exit()
+    {
+        
+    }
+}
