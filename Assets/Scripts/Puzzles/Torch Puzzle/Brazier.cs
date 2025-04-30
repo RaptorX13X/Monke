90fcae0aba6455141c6e61e24ceaaabb2dc8d0dc
Raptor <panraptor13@gmail.com>
2025-04-30 21:27:57 +0200

    .

--- Assets/Scripts/Puzzles/Torch Puzzle/Brazier.cs
+++ Assets/Scripts/Puzzles/Torch Puzzle/Brazier.cs
@@ -37,6 +37,7 @@ public class Brazier : MonoBehaviour
 
         private void Interact()
         {
+            if (!stateMachine.hasTorch) return;
             if (canInteract)
             {
                 burning = true;
