6db5d40870a8047e286bd0eb3fd4b538c2aa65a2
Aastonica <julka.sekretarczyk@gmail.com>
2025-05-08 23:19:49 +0200

    menu music
    yaay

---
+++ Assets/Scripts/Audio/MenuAudioManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using FMODUnity;
+
+public class MenuAudioManager : MonoBehaviour
+{
+    //[SerializeField] private StudioEventEmitter emitter;
+
+    //public void StopMenuMusic()
+    //{
+    //    emitter.Stop();
+    //}
+}
