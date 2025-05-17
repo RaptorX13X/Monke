aa0b95de03a915cb75462f78ebcf0ff3649f53f9
Aastonica <julka.sekretarczyk@gmail.com>
2025-05-17 15:00:27 +0200

    spikes

---
+++ Assets/Scripts/Audio/SpikesAudio.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using FMODUnity;
+
+public class SpikesAudio : MonoBehaviour
+{
+  
+    [SerializeField] private Spikes spikes;
+    [SerializeField] private StudioEventEmitter EmitterRise;
+    [SerializeField] private StudioEventEmitter EmitterHide;
+
+
+    private void Start()
+    {
+        spikes.rise += PlaySpikesRise;
+        spikes.hide += PlaySpikesHide;
+    }
+    public void PlaySpikesRise()
+    {
+        EmitterRise.Play();
+    }
+
+    public void PlaySpikesHide()
+    {
+        EmitterHide.Play();
+    }
+
+    private void OnDestroy()
+    {
+        spikes.rise -= PlaySpikesRise;
+        spikes.hide -= PlaySpikesHide;
+    }
+}
